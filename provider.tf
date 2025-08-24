9eb11b2997bd3d36d82936d9744f2bb819af405c
Hariom <155458286+hariomsingh945@users.noreply.github.com>
2025-08-24 19:51:55 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,10 @@
 terraform {
+backend "azurerm" {
+    resource_group_name  = "RG_Backend"
+    storage_account_name = "madarikakhel"           # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
